8ac58ce0ab35feeb050cc7141cb6747ff35a1b27
vvvalm <nizamievvalmir@gmail.com>
2023-02-19 20:51:58 +0700

    Create Functions

---
+++ Geometry.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 100
+
+void handle_error(const char* error_msg, const char* input, int pos)
+{
+}
+int check_circle(char* input)
+{
+}
+int main(int argv, char** argc)
+{
+
+    fclose(file);
+    return 0;
+}
